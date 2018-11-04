58c0433b2709c71451c03f1d7166c33298b6eee0
James <j.froggatt-15@student.lboro.ac.uk>
2018-11-04 11:34:06 +0000

    Initial Commit

---
+++ Hive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hive
+//
+//  Created by James Froggatt on 04.11.2018.
+//  Copyright © 2018 James Froggatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
